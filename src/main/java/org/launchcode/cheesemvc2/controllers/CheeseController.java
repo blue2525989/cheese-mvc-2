55e93fb619df9a05e37f9b50ed88c9c2358dd8ea
jason <jbierbrauer99@gmail.com>
2017-05-17 09:06:16 -0500

    studio 4 complete

--- src/main/java/org/launchcode/cheesemvc2/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc2/controllers/CheeseController.java
@@ -1,5 +1,6 @@
 package org.launchcode.cheesemvc2.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.springframework.stereotype.Controller;
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping(value="cheese")
 public class CheeseController {
 
-	static HashMap<String, String> cheese = new HashMap<>();
+	static HashMap<String, String> cheeses = new HashMap<>();
 	
 	@RequestMapping(value="")
 	public String index(Model model) {
-		model.addAttribute("cheeses", cheese);		
+		model.addAttribute("cheeses", cheeses);		
 		String title = "My Cheeses";
 		model.addAttribute("title", title);
 		return "cheese/index";
@@ -31,9 +32,26 @@ public class CheeseController {
 	@RequestMapping(value="add", method=RequestMethod.POST)
 	public String processAddCheeseForm(Model model, @RequestParam String cheeseName,
 			@RequestParam String description) {
-		cheese.put(cheeseName, description);
+		cheeses.put(cheeseName, description);
 		model.addAttribute("title", "Add Cheese");
 		// redirect is relative to top level request mapping
+		return "redirect:";
+	}
+	
+	@RequestMapping(value="remove", method=RequestMethod.GET)
+	public String removeForm(Model model) {
+		model.addAttribute("cheeses", cheeses);
+		model.addAttribute("title", "Remove a Cheese");
+		return "cheese/remove";
+	}
+	
+	@RequestMapping(value="remove", method=RequestMethod.POST)
+	public String processRemoveForm(Model model, @RequestParam ArrayList<String> cheese) {
+		for (int i = 0; i < cheese.size(); i++) {
+			if (cheeses.containsKey(cheese.get(i))) {
+				cheeses.remove(cheese.get(i));
+			}
+		}
 		return "redirect:";
 	}
 }
